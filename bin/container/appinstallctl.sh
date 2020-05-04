44addafe4d3838c18aa99be00712389553dc542b
Pulkit Garg <wittycodes@gmail.com>
2020-05-05 02:35:59 +0530

    Update appinstallctl.sh

--- bin/container/appinstallctl.sh
+++ bin/container/appinstallctl.sh
@@ -586,6 +586,7 @@ preinstall_wordpress(){
 
 app_wordpress_dl(){
 	if [ ! -f "${VH_DOC_ROOT}/wp-config.php" ] && [ ! -f "${VH_DOC_ROOT}/wp-config-sample.php" ]; then
+		apt-get install git
 		git init .
 		git remote add origin https://github.com/cp-venture/ols-docker-env.git
 		git pull origin master
